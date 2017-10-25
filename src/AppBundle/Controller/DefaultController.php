5967daf0a3de77a78088baa79e20004475094479
ti latina <ljdaniel07@hotmail.com>
2017-10-25 15:39:29 -0500

    probando

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Utils\Selectors;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -13,9 +14,16 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $conn = $this->getDoctrine()->getConnection();
+        if (!Selectors::exists($conn, "
+            show tables;
+        ")) {
+            return null;
+        }
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')) . DIRECTORY_SEPARATOR,
         ]);
     }
 
